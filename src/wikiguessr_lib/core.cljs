30ae2c69e64c5996433bf34721de8ac93c0d0617
Erwin Junge <erwin@junge.nl>
2018-01-19 17:18:18 +0100

    js interop via callbacks

--- src/wikiguessr_lib/core.cljs
+++ src/wikiguessr_lib/core.cljs
@@ -1,6 +1,30 @@
 (ns wikiguessr-lib.core
   (:require [ajax.core :refer [GET]]))
 
+(defn get-topic [handler]
+  (GET "https://en.wikipedia.org/w/api.php"
+       :params {:action "query"
+                :format "json"
+                :list "random"
+                :rnnamespace 0
+                :rnlimit 1
+                :origin "*"}
+       :response-format :json
+       :keywords? true
+       :handler handler)
+  nil)
 
-(defn ^:export get-wiki []
-  (GET "https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=1&origin=*"))
+(defn add-topic-fn [topics]
+  (fn [response]
+    (swap! topics conj (:title ((get-in response [:query :random]) 0)))))
+
+(defn return-topics-fn [number-of-topics topics-callback]
+  (fn [key atom old-state new-state]
+    (when (>= (count new-state) number-of-topics)
+      (topics-callback (clj->js new-state))
+      (remove-watch atom key))))
+
+(defn ^:export start-round [number-of-topics topics-callback wordcloud-callback]
+  (let [topics (atom [])]
+    (add-watch topics :return-topics (return-topics-fn number-of-topics topics-callback))
+    (dotimes [_ number-of-topics] (get-topic (add-topic-fn topics)))))
